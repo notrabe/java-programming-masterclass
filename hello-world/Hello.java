93fcfa2f0ee81a26ade9ad5d92ece49ff9ff4531
Evan Barton <notrabe1011@gmail.com>
2021-01-25 18:02:39 -0500

    print number variable

--- hello-world/Hello.java
+++ hello-world/Hello.java
@@ -1,5 +1,8 @@
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("hello world");
+        System.out.println("Hello Evan");
+
+        int myFirstNumber = 5;
+        System.out.println(myFirstNumber);
     }
 }
