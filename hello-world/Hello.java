e099eba1a3f8d6c1c08147cbee501c3c3b365ddd
Evan Barton <notrabe1011@gmail.com>
2021-01-25 18:13:36 -0500

    starting out with expressions

--- hello-world/Hello.java
+++ hello-world/Hello.java
@@ -2,7 +2,12 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello Evan");
 
-        int myFirstNumber = 5;
-        System.out.println(myFirstNumber);
+        int myFirstNumber = (10 + 5) + (2 * 10);
+        int mySecondNumber = 12;
+        int myThirdNumber = 6;
+        int myTotal = myFirstNumber + mySecondNumber + myThirdNumber; 
+        int myLastOne = myTotal - 1000;
+        System.out.println(myTotal);
+        System.out.println(myLastOne);
     }
 }
