73964f1a0d521da2af7e1a2cf5437f2abadd344f
Evan Barton <notrabe1011@gmail.com>
2021-02-05 23:22:54 -0500

    char and boolean

---
+++ CharAndBoolean/Main.java
@@ -0,0 +1,16 @@
+package CharAndBoolean;
+
+public class Main {
+    public static void main(String[] args){
+        char myChar = 'D';
+        char myUnicodeChar = '\u0044';
+        System.out.println(myChar);
+        System.out.println(myUnicodeChar);
+        char myCopyrightChar = '\u00A9';
+        System.out.println(myCopyrightChar);
+        boolean myTrueBoolean = true;
+        System.out.println(myTrueBoolean);
+        boolean myFalseBoolean = false;
+        System.out.println(myFalseBoolean);
+    }
+}
