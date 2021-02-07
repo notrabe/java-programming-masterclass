08f90783765efaa92028835f2858dede4607a616
Evan Barton <notrabe1011@gmail.com>
2021-02-07 17:59:57 -0500

    keywords and expressions

--- KeywordsAndExpressions/Main.java
+++ KeywordsAndExpressions/Main.java
@@ -1,7 +1,14 @@
 package KeywordsAndExpressions;
 
+import sun.awt.geom.AreaOp.IntOp;
+
 public class Main {
 	public static void main(String[] args) {
+		double kilometers = (100 * 1.609344);
 		
+		int highScore = 50;
+		if(highScore == 50) {
+			System.out.println("This is an expression");
+		}
 	}
 }
