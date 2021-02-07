23b87942bfa25eb32010fef73a65f594f92309bc
Evan Barton <notrabe1011@gmail.com>
2021-02-07 02:25:11 -0500

    added comments

--- Operators/Main.java
+++ Operators/Main.java
@@ -1,39 +1,43 @@
 package Operators;
 
 public class Main {
-    public static void main(String[] args) {
-        int result = 1 + 2;
-        System.out.println("1 + 2 = " + result);
-        int prevResult = result;
-        System.out.println("previous result = "  + prevResult);
-        result = result - 1;
-        System.out.println("3 - 1 = " + result);
-        System.out.println("previous result = "  + prevResult);
+	public static void main(String[] args) {
+		int result = 1 + 2;
+		System.out.println("1 + 2 = " + result);
+		int prevResult = result;
+		System.out.println("previous result = "  + prevResult);
+		result = result - 1;
+		System.out.println("3 - 1 = " + result);
+		System.out.println("previous result = "  + prevResult);
 
-        result = result * 10;
-        System.out.println("2 * 10 = " + result);
+		result = result * 10;
+		System.out.println("2 * 10 = " + result);
 
-        result = result / 5;
-        System.out.println("20 / 5 = " + result);
+		result = result / 5;
+		System.out.println("20 / 5 = " + result);
 
-        result = result % 3; // what is left over after 3 is put into the remainder? in this case it is 1.
-        System.out.println("4 % 3 = " + result);
+		result = result % 3; // what is left over after 3 is put into the remainder? in this case it is 1.
+		System.out.println("4 % 3 = " + result);
 
-        //result = result + 1
-        result++;
-        System.out.println("1 + 1 = " + result);
+		//result = result + 1
+		result++;
+		System.out.println("1 + 1 = " + result);
 
-        result--;
-        System.out.println("2 - 1 = " + result);
+		//result = result - 1
+		result--;
+		System.out.println("2 - 1 = " + result);
+		
+		//result = result + 2 
+		result += 2;
+		System.out.println("1 + 2 = " + result);
 
-        result += 2;
-        System.out.println("1 + 2 = " + result);
-
-        //result = result * 10
-        result *= 10;
-        System.out.println("3 * 10 = " + result);
-        
-        result /= 10;
-        System.out.println("30 / 10 = " + result);
-    }
+		//result = result * 10
+		result *= 10;
+		System.out.println("3 * 10 = " + result);
+		
+		//result = result / 10
+		result /= 10;
+		System.out.println("30 / 10 = " + result);
+		
+	}
 }
