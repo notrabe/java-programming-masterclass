10d289294d647bf21c8d44ba4e3fdc593fdfed37
Evan Barton <notrabe1011@gmail.com>
2021-02-09 23:30:45 -0500

    if keywords and codeblocks challenge

--- IfKeywordAndCodeblocks/Main.java
+++ IfKeywordAndCodeblocks/Main.java
@@ -1,6 +1,6 @@
 package IfKeywordAndCodeblocks;
 
-import sun.awt.geom.AreaOp.IntOp;
+import javax.swing.text.StyledEditorKit.BoldAction;
 
 public class Main{
 	public static void main(String[] args) {
@@ -9,8 +9,28 @@ public class Main{
 		int levelCompleted = 5;
 		int bonus = 100;
 		
-		if(score == 5000) {
-			System.out.println("Your score was 5000");
+//		if(score < 5000 && score > 1000) {
+//			System.out.println("Your score was less than 5000 but greater than 1000");
+//		} else if (score < 1000) {
+//			System.out.println("Your scre was less than 1000");
+//		} else {
+//			System.out.println("Got here");
+//		}
+		
+		if(gameOver == true) {
+			int finalScore = score + (levelCompleted * bonus);
+			System.out.println("Your final score was " + finalScore);
 		}
+
+		boolean gameOver2 = true;
+		int score2 = 10000;
+		int levelCompleted2 = 8;
+		int bonus2 = 200;
+
+		if(gameOver2 == true) {
+			int finalScore = score2 + (levelCompleted2 * bonus2);
+			System.out.println("Your final score was " + finalScore);
+		}
+
 	}
 }
