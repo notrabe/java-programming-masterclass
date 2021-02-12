a88701b625bccdfab08dbf620c4b1ac8b48756ea
Evan Barton <notrabe1011@gmail.com>
2021-02-12 00:03:42 -0500

    methods challenge

--- Methods/Main.java
+++ Methods/Main.java
@@ -1,16 +1,39 @@
 package Methods;
 
 public class Main {
+
     public static void main(String[] args) {    
-        calculateScore(true, 800, 5, 100);
-        calculateScore(true, 10000, 8, 200);   
+        displayHighScorePosition("Evan", calculateHighScorePosition(1500));
+        displayHighScorePosition("Bill", calculateHighScorePosition(900));
+        displayHighScorePosition("Bob", calculateHighScorePosition(400));
+        displayHighScorePosition("Joe", calculateHighScorePosition(50));
+        System.out.println("Your final score was " + calculateScore(true, 800, 5, 100)); 
+        System.out.println("Your final score was " + calculateScore(true, 10000, 8, 200));    
     }
 
-    public static void calculateScore(boolean gameOver, int score, int levelCompleted, int bonus) {
+    public static int calculateScore(boolean gameOver, int score, int levelCompleted, int bonus) {
         if(gameOver == true) {
             int finalScore = score + (levelCompleted * bonus);
             finalScore += 1000;
-            System.out.println("Your final score was " + finalScore);
-        }      
+            return finalScore;
+        }
+        return -1;      
+    }
+
+    public static  void displayHighScorePosition(String name, int position) {
+        System.out.println(name + " managed to get into position " + position);
+    }
+
+    public static int calculateHighScorePosition(int score) {
+        if(score > 1000){
+            return 1;
+        }
+        if(score > 500 && score < 1000){
+            return 2;
+        }
+        if(score > 100 && score < 500){
+            return 3;
+        }
+        return 4;
     }
 }
