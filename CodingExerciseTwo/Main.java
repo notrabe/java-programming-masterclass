50152d536ddf33a4bdc1f60d2229f659cfa435c1
Evan Barton <notrabe1011@gmail.com>
2021-02-13 11:14:02 -0500

    coding exercise two

---
+++ CodingExerciseTwo/Main.java
@@ -0,0 +1,18 @@
+package CodingExerciseTwo;
+
+public class Main {
+	public static void main(String[] args) {
+		printMegaBytesAndKiloBytes(2500);
+		printMegaBytesAndKiloBytes(-1024);
+		printMegaBytesAndKiloBytes(5000);
+	}
+	public static void printMegaBytesAndKiloBytes(int kiloBytes) {
+		if(kiloBytes < 0) {
+			System.out.println("Invalid Value");
+		} else {
+			int megaBytes = (int)kiloBytes / 1024;
+			int remainder = kiloBytes % 1024;  
+			System.out.println(kiloBytes + " KB = " + megaBytes + " MB and " + remainder + " KB");
+		}
+	}
+}
