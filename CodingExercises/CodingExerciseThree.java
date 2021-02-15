43b8f9ddfbc64487ccf076cd4a225a0f42edcf72
Evan Barton <notrabe1011@gmail.com>
2021-02-14 21:07:39 -0500

    coding exercise three

---
+++ CodingExercises/CodingExerciseThree.java
@@ -0,0 +1,22 @@
+package CodingExercises;
+
+
+public class CodingExerciseThree {
+	public static void main(String[] args) {
+		System.out.println(shouldWakeUp(false, 2));
+		System.out.println(shouldWakeUp(true, 1));
+		System.out.println(shouldWakeUp(false, 2));
+		System.out.println(shouldWakeUp(true, -1));
+	}
+	public static boolean shouldWakeUp(boolean barking, int hourOfDay) {
+		 if (barking == false) {
+			 return false;
+		 } else if (hourOfDay < 0 || hourOfDay > 23) {
+			 return false;
+		 } else if(barking == true && hourOfDay < 8 || barking == true && hourOfDay > 22) {
+			 return true;
+		 } else {
+			 return false;
+		 }
+	}
+}
