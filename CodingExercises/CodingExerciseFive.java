ef7102f8d824ada3b0fa9d3dce31612d8b5a5bc8
Evan Barton <notrabe1011@gmail.com>
2021-02-14 22:36:20 -0500

    coding exercise five

---
+++ CodingExercises/CodingExerciseFive.java
@@ -0,0 +1,20 @@
+package CodingExercises;
+
+public class CodingExerciseFive {
+	public static void main(String[] args) {
+		System.out.println(areEqualByThreeDecimalPlaces(-3.1756, -3.175));
+		System.out.println(areEqualByThreeDecimalPlaces(3.175, 3.176));
+		System.out.println(areEqualByThreeDecimalPlaces(3.0, 3.0));
+	}
+	
+	public static boolean areEqualByThreeDecimalPlaces(double num1, double num2) {
+		double num1ToInt = num1 * 1000;
+		double num2ToInt = num2 * 1000;
+		
+		if((int)num1ToInt == (int)num2ToInt) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+}
