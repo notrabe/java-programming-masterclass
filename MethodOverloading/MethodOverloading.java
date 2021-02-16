28f83d52268bbb38e2aa1de69782da2ff1aa8cd8
Evan Barton <notrabe1011@gmail.com>
2021-02-15 21:44:24 -0500

    method overloading challenge

--- MethodOverloading/MethodOverloading.java
+++ MethodOverloading/MethodOverloading.java
@@ -7,7 +7,9 @@ public class MethodOverloading {
 		int newScore = calculateScore("Evan", 500);
 		System.out.println("New score is " + newScore);
 		calculateScore(75);
-		calculateScore(); 
+		calculateScore();
+		System.out.println(calcFeetAndInchesToCentimeters(1, 10));
+		System.out.println(calcFeetAndInchesToCentimeters(49));
 	}
 	
 	public static int calculateScore(String playerName, int score) {
@@ -23,5 +25,31 @@ public class MethodOverloading {
 	public static int calculateScore() {
 		System.out.println("No player name, no score.");
 		return 0;
+	}
+	
+	public static double calcFeetAndInchesToCentimeters(int feet, int inches) {
+		if (feet < 0 || inches < 0) {
+			return -1;
+		}
+		
+		int feetToInches = feet * 12;
+		int totalInches = feetToInches + inches;
+		double centimeters = totalInches * 2.54;
+		
+		return centimeters;
+	}
+	
+	public static double calcFeetAndInchesToCentimeters(int inches) {
+		if (inches < 0) {
+			return -1;
+		}
+		
+		double centimeters = inches * 2.54;
+		int feet = inches / 12;
+		int remainingInches = inches % 12;
+
+		calcFeetAndInchesToCentimeters(feet, remainingInches);
+		
+		return centimeters;
 	}
 }
