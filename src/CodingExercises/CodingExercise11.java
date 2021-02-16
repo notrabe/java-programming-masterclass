37f864fbbea30b3946d5472a72c4f13ffcf279c7
Evan Barton <notrabe1011@gmail.com>
2021-02-15 23:49:51 -0500

    coding exercise 11

---
+++ src/CodingExercises/CodingExercise11.java
@@ -0,0 +1,27 @@
+package CodingExercises;
+
+public class CodingExercise11 {
+	public static void main(String[] args) {
+		System.out.println(isCatPlaying(true, 10));
+		System.out.println(isCatPlaying(false, 36));
+		System.out.println(isCatPlaying(false, 35));
+	}
+	
+	public static boolean isCatPlaying(boolean summer, int temperature) {
+		if (summer == true) {
+			if(temperature > 45 || temperature < 25) {
+				return false;
+			} else {
+				return true;
+			}
+		} else if(summer == false) {
+			if(temperature > 35 || temperature < 25) {
+				return false;
+			} else {
+				return true;
+			}
+		} else {
+			return false;
+		}
+	}
+}
