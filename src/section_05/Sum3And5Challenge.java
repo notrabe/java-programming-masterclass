9e5b3255953052f3207f979c878d70364ac16f15
Evan Barton <notrabe1011@gmail.com>
2021-02-16 21:26:48 -0500

    sum 3 and 5 challenge

---
+++ src/section_05/Sum3And5Challenge.java
@@ -0,0 +1,20 @@
+package section_05;
+
+public class Sum3And5Challenge {
+	public static void main(String[] args) {
+		int sum = 0;
+		int count = 0;
+		
+		for(int i = 1; i <= 1000; i++) {
+			if(i %  3 == 0 && i % 5 == 0) {
+				count++;
+				sum = sum + i;
+				System.out.println("Found number = " + i);
+				if(count == 5) {
+					System.out.println(sum);
+					break;
+				}
+			}
+		}
+	}
+}
