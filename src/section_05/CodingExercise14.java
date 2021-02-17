f45aabfe2dd16b9c9880468658c12d7c338fb24d
Evan Barton <notrabe1011@gmail.com>
2021-02-16 21:51:18 -0500

    coding exercise 14

---
+++ src/section_05/CodingExercise14.java
@@ -0,0 +1,41 @@
+package section_05;
+
+public class CodingExercise14 {
+	public static void main(String[] args) {
+//		System.out.println(isOdd(-1));
+//		System.out.println(isOdd(5));
+//		System.out.println(isOdd(8));
+		
+		System.out.println(sumOdd(1, 100));
+		System.out.println(sumOdd(-1, 100));
+		System.out.println(sumOdd(100, 100));
+		System.out.println(sumOdd(13, 13));
+		System.out.println(sumOdd(100, -100));
+		System.out.println(sumOdd(100, 1000));
+	}
+	
+	public static boolean isOdd(int number) {
+		if(number < 0) {
+			return false;
+		}
+		
+		if(number % 2 == 0) {
+			return false;
+		} else {
+			return true;
+		}
+	}
+	
+	public static int sumOdd(int start, int end) {
+		int sum = 0;
+		if(start < 0 || end < 0 || end < start) {
+			return -1;
+		}
+		for(int i = start; i <= end; i++) {
+			if(isOdd(i)) {
+				sum += i;
+			}
+		}
+		return sum;
+	}
+}
