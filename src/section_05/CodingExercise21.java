f9b6339e91fe38ec489e6853bdec308ed9022fc8
Evan Barton <notrabe1011@gmail.com>
2021-02-17 19:16:15 -0500

    coding exercise 21

---
+++ src/section_05/CodingExercise21.java
@@ -0,0 +1,19 @@
+package section_05;
+
+public class CodingExercise21 {
+	public static void main(String[] args) {
+		printFactors(6);
+	}
+	
+	public static void printFactors(int number) {
+		if(number<1) {
+			System.out.println("Invalid Value");
+		}
+		
+		for(int i=1;i<=number;i++) {
+			if(number%i==0) {
+				System.out.println(i);
+			}
+		}
+	}
+}
