6b517027385704ed87ca287ffad12c09e25e70df
Evan Barton <notrabe1011@gmail.com>
2021-02-21 03:33:10 +0000

    refactor section 6

--- src/section_06/coding_exercises/CodingExercise29.java
+++ src/section_06/coding_exercises/CodingExercise29.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package section_06.Classes.coding_exercises;
+package section_06.coding_exercises;
 
 /**
  *
