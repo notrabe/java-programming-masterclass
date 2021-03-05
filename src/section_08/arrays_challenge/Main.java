c5a8f12210dbffa46f7989120b92ab3a9b37f52e
Evan Barton <notrabe1011@gmail.com>
2021-03-04 20:54:56 -0500

    array god I hate listening to this man breathe

--- src/section_08/arrays_challenge/Main.java
+++ src/section_08/arrays_challenge/Main.java
@@ -21,39 +21,28 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
  * THE SOFTWARE.
  */
-package section_08.arrays;
-
-import java.util.Scanner;
+package section_08.arrays_challenge;
 
 /**
  *
  * @author notrabe <github.com/notrabe>
  */
 public class Main {
-
 	private static Scanner scanner = new Scanner(System.in);
-	public static void main(String[] args) {
-
-		int[] myInts = getInts(5);
-		for(int i=0; i< myInts.length; i++){
-			System.out.println("Element " + i +", type value was " +myInts[i]);
-		}
-		System.out.println("The average is " +getAverage(myInts));
 	
+	public static void main(String[] args) {
+		
+		int[] myInts = getInts(5);
 	}
-	public static int [] getInts(int number) {
-		System.out.println("Enter "+number+" integer values. \r");
-		int[] values = new int[number];
-		for(int i=0; i<values.length; i++){
-			values[i] = scanner.nextInt();
+
+	public static int [] getInts(int capacity) {
+		int[] array = new int[capacity];
+		System.out.println("Enter" +capacity + " integer values: \r");
+		for(int i = 0; i<array.length; i++){
+			array[i] = scanner.nextInt();
 		}
-		return values;
+		return array;
 	}
-	public static double getAverage(int[] array){
-		int sum = 0;
-		for(int i=0;i<array.length;i++){
-			sum += array[i];
-		}
-		return (double) sum / (double)array.length;
-	}
+
+	public static void printArray()
 }
